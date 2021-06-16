42764bc699f71d35bf5441324455bc76434817e5
Dillon Lui <dillonglui@gmail.com>
2021-06-15 20:16:18 -0500

    updated item detail to use new seed items

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,20 +1,44 @@
-import React, { useState, useEffect } from "react"
-import { Link } from "react-router-dom"
+import React, { useState } from "react"
+import { Link, useHistory } from "react-router-dom"
+import styles from "./ItemDetail.module.css"
+import { BsChevronLeft } from "react-icons/bs"
 
-const ItemDetail = () => {
-    useEffect(() => {
-        fetchItem()
-    }, [])
+function ItemDetail({ item, addCartItem }) {
+    const [showCompleteOrder, setShowCompleteOrder] = useState(false)
+    const { goBack } = useHistory()
 
-    const [item, setItem] = useState([])
-
-    const fetchItem = async () => {
-
+    const handleAdd = () => {
+        setShowCompleteOrder(true)
+        addCartItem(item)
     }
 
     return (
-        <div>
-            <h1>Item Page</h1>
+        <div className={styles.root}>
+            <img
+                className={styles.image}
+                src={`https://source.unsplash.com/${item.imageId}/600x700`}
+                alt={item.id}
+            />
+            <div className={styles['info-container']}>
+                <div className={styles.info}>
+                    <h1>{item.itemName}</h1>
+                    <p>{item.price.toFixed(2)} USD</p>
+
+                    <button className={styles['black-btn']} onClick={handleAdd}>
+                        Add to Cart
+          </button>
+
+                    {showCompleteOrder && (
+                        <Link to="/cart">
+                            <button className={styles['black-btn']}>Complete Order</button>
+                        </Link>
+                    )}
+
+                    <button className={styles['go-back-btn']} onClick={goBack}>
+                        <BsChevronLeft /> Go Back
+          </button>
+                </div>
+            </div>
         </div>
     )
 }
