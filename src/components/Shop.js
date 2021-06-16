f81db87e328d9cf5fc393485a4b10595e8037aae
Dillon Lui <dillonglui@gmail.com>
2021-06-15 20:16:56 -0500

    updated shop page to use seed items and ShopItem

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,66 +1,11 @@
-import React, { useState, useEffect } from "react"
-import { Link } from "react-router-dom"
+import React from "react"
+import styles from "./Shop.module.css"
+import ShopItem from "./ShopItem"
 
-const Shop = () => {
-    useEffect(() => {
-        fetchItems()
-    }, [])
-
-    const [items, setItems] = useState([])
-    const [cart, setCart] = useState([])
-
-    const fetchItems = async () => {
-        const data = await fetch('https://fakestoreapi.com/products?limit=10')
-            .then(res => res.json())
-        setItems(data)
-        console.log(data)
-    }
-
-    const handleAdd = (e) => {
-        let itemId = e.target.parentNode.dataset.id
-        let newCart = cart
-        newCart.push(items[itemId - 1])
-        setCart(newCart)
-        console.log(newCart)
-    }
-
+const Shop = ({ items }) => {
     return (
-        <div>
-            <div className="shopContainer">
-                <div className="shopItems">
-                    <h2>Products</h2>
-                    <div className="shopItemList">
-                        {items.map(item => {
-                            return (
-                                <div className="itemCard" key={item.id} data-id={item.id}>
-                                    <Link to={`/shop/${item.id}`}>
-                                        <h3>{item.title}</h3>
-                                        <div className="itemImg">
-                                            <img src={item.image} alt={item.title} />
-                                        </div>
-                                    </Link>
-                                    <button onClick={handleAdd}>Add to Cart</button>
-                                </div>
-                            )
-                        })}
-                    </div>
-
-                </div>
-                <div className="shopCart">
-                    <h2>Cart</h2>
-                    <div>
-                        {cart.map(item => {
-                            return (
-                                <div>
-                                    <h3>{item.title}</h3>
-                                </div>
-                            )
-                        })
-                        }
-                    </div>
-                </div>
-            </div>
-
+        <div className={styles.root}>
+            {items.map((item) => <ShopItem key={item.id}{...item} />)}
         </div>
     )
 }
